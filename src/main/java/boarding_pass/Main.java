0932f0664647ebc55e4edeab6c85747f4d05dceb
pclifton <pclifton739@gmail.com>
2022-05-04 13:14:26 -0500

    Update diagram, added PassengerInfo class

--- src/main/java/boarding_pass/Main.java
+++ src/main/java/boarding_pass/Main.java
@@ -2,9 +2,7 @@ package boarding_pass;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Test 2");
-        System.out.println("Test 3");
+
     }
 
 }
---
+++ src/main/java/boarding_pass/PassengerInfo.java
@@ -0,0 +1,9 @@
+package boarding_pass;
+
+import java.util.Scanner;
+
+public class PassengerInfo {
+    Scanner scanner = new Scanner(System.in);
+    String firstName;
+    String lastName;
+}
