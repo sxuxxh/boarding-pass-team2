948227dd8fc0776501da0ac99b9a3ca625eb64cc
pclifton <pclifton739@gmail.com>
2022-05-09 14:34:44 -0500

    Fixed Male method in CalculateTotalPrice
    Bug was causing method to add multiple discounts, displaying a negative number. Moved method to top to be tested first.

--- src/main/java/boarding_pass/CalculateTotalPrice.java
+++ src/main/java/boarding_pass/CalculateTotalPrice.java
@@ -20,31 +20,30 @@ public class CalculateTotalPrice {
         if (key.equals(tripCode)){
             tripCost = BoardingPass.priceList.getPriceLst().get(key);
         }
-        if (PassengerInfo.gender.equals("Female") || PassengerInfo.gender.equals("female")){
+        if (PassengerInfo.gender.equals("Male") || PassengerInfo.gender.equals("male")) {
+            afterDiscountCost = tripCost;
+        }
+        if (PassengerInfo.gender.equals("Female")){
             discount = tripCost * 0.25;
             afterDiscountCost = tripCost - discount;
         }
         if(PassengerInfo.age <= 12){
+            discount = tripCost * 0.50;
             if (afterDiscountCost != 0){
-                discount = tripCost * 0.50;
                 afterDiscountCost = afterDiscountCost - discount;
+
             } else {
-                discount = tripCost * 0.50;
                 afterDiscountCost = tripCost - discount;
             }
 
         }
         if (PassengerInfo.age >= 60) {
+            discount = tripCost * 0.60;
             if (afterDiscountCost != 0){
-                discount = tripCost * 0.60;
                 afterDiscountCost = afterDiscountCost - discount;
             } else {
-                discount = tripCost * 0.60;
                 afterDiscountCost = tripCost - discount;
             }
-        }
-        if(PassengerInfo.gender.equals("Male") || PassengerInfo.gender.equals("male") && PassengerInfo.age > 12 && PassengerInfo.age < 60){
-            afterDiscountCost = tripCost;
         }
      }
 
