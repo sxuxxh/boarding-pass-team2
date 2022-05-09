99913061d4db44087353f4fbc6d3d11dca6cd1e5
pclifton <pclifton739@gmail.com>
2022-05-09 10:16:57 -0500

    Fix double return in CalculateTotalPrice

--- src/main/java/boarding_pass/CalculateTotalPrice.java
+++ src/main/java/boarding_pass/CalculateTotalPrice.java
@@ -19,20 +19,20 @@ public class CalculateTotalPrice {
         }
         if(PassengerInfo.age <= 12){
             if (afterDiscountCost != 0){
-                discount = tripCost * 0.5;
+                discount = tripCost * 0.50;
                 afterDiscountCost = afterDiscountCost - discount;
             } else {
-                discount = tripCost * 0.5;
+                discount = tripCost * 0.50;
                 afterDiscountCost = tripCost - discount;
             }
 
         }
         if (PassengerInfo.age >= 60) {
             if (afterDiscountCost != 0){
-                discount = tripCost * 0.6;
+                discount = tripCost * 0.60;
                 afterDiscountCost = afterDiscountCost - discount;
             } else {
-                discount = tripCost * 0.6;
+                discount = tripCost * 0.60;
                 afterDiscountCost = tripCost - discount;
             }
         }
